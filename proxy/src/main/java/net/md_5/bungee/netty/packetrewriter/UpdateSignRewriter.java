a926110db54357e65d66a5b38119e9c50b75514d
TheUnnamedDude <kevin.sillerud@gmail.com>
2013-11-26 09:09:26 +0100

    Refix signs

--- proxy/src/main/java/net/md_5/bungee/netty/packetrewriter/UpdateSignRewriter.java
+++ proxy/src/main/java/net/md_5/bungee/netty/packetrewriter/UpdateSignRewriter.java
@@ -9,21 +9,17 @@ public class UpdateSignRewriter extends PacketRewriter
     @Override
     public void rewriteClientToServer(ByteBuf in, ByteBuf out)
     {
-        out.writeInt( in.readInt() );
-        out.writeShort( in.readShort() );
-        out.writeInt( in.readInt() );
+        out.writeBytes( in.readBytes( 66 ) ); // int, short, int
         for ( int i = 0; i < 4; i ++ )
         {
-            Var.writeString( Var.readString( in, false ), out, true );
+            Var.writeString( Var.readString( in, true ), out, false );
         }
     }
 
     @Override
     public void rewriteServerToClient(ByteBuf in, ByteBuf out)
     {
-        out.writeInt( in.readInt() );
-        out.writeShort( in.readShort() );
-        out.writeInt( in.readInt() );
+        out.writeBytes( in.readBytes( 66 ) ); // int, short, int
         for ( int i = 0; i < 4; i ++ )
         {
             Var.writeString( Var.readString( in, false ), out, true );

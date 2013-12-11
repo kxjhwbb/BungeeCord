363619194b1a5f3543bb87c1ef11f9d7b27650d5
TheUnnamedDude <kevin.sillerud@gmail.com>
2013-12-11 09:20:26 +0100

    Formatting

--- proxy/src/main/java/net/md_5/bungee/netty/packetrewriter/ItemDataRewriter.java
+++ proxy/src/main/java/net/md_5/bungee/netty/packetrewriter/ItemDataRewriter.java
@@ -7,15 +7,17 @@ public class ItemDataRewriter extends PacketRewriter
 {
 
     @Override
-    public void rewriteClientToServer(ByteBuf in, ByteBuf out) {
+    public void rewriteClientToServer(ByteBuf in, ByteBuf out)
+    {
         unsupported( true );
     }
 
     @Override
-    public void rewriteServerToClient(ByteBuf in, ByteBuf out) {
+    public void rewriteServerToClient(ByteBuf in, ByteBuf out)
+    {
         in.skipBytes( 2 ); // Short - itemtype
         short itemDamage = in.readShort();
-        Var.writeVarInt( itemDamage, out );
+        Var.writeVarInt(itemDamage, out);
         out.writeBytes( in.readBytes( in.readableBytes() ) );
     }
 

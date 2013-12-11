9ccba69e3ad534bc9984427fd0288fced0b047e0
TheUnnamedDude <kevin.sillerud@gmail.com>
2013-12-11 09:17:31 +0100

    Fix maps not displaying correctly

---
+++ proxy/src/main/java/net/md_5/bungee/netty/packetrewriter/ItemDataRewriter.java
@@ -0,0 +1,22 @@
+package net.md_5.bungee.netty.packetrewriter;
+
+import io.netty.buffer.ByteBuf;
+import net.md_5.bungee.netty.Var;
+
+public class ItemDataRewriter extends PacketRewriter
+{
+
+    @Override
+    public void rewriteClientToServer(ByteBuf in, ByteBuf out) {
+        unsupported( true );
+    }
+
+    @Override
+    public void rewriteServerToClient(ByteBuf in, ByteBuf out) {
+        in.skipBytes( 2 ); // Short - itemtype
+        short itemDamage = in.readShort();
+        Var.writeVarInt( itemDamage, out );
+        out.writeBytes( in.readBytes( in.readableBytes() ) );
+    }
+
+}

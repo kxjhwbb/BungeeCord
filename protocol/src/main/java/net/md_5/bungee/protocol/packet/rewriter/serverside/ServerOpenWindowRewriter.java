9b178e1890c273a67843eb6e3cbebbe6de7cf8cb
TheUnnamedDude <kevin.sillerud@gmail.com>
2014-01-27 22:02:43 +0100

    Fix chests, enchantment tables are broken

--- protocol/src/main/java/net/md_5/bungee/protocol/packet/rewriter/serverside/ServerOpenWindowRewriter.java
+++ protocol/src/main/java/net/md_5/bungee/protocol/packet/rewriter/serverside/ServerOpenWindowRewriter.java
@@ -6,24 +6,33 @@ import net.md_5.bungee.protocol.packet.rewriter.PacketRewriter;
 
 public class ServerOpenWindowRewriter extends PacketRewriter
 {
-    String[] inventoryTypes = {
-        "Chest/Large chest",
-            "Workbench",
-            "Furnace",
-            "Dispenser",
-            "Enchantment table",
-            "Brewing stand",
-            "Npc trade",
-            "Beacon",
-            "Anvil",
-            "Hopper"
-    };
+    static String[] inventoryTypes = new String[ 0xFF ];
+    static
+    {
+        inventoryTypes[ 0 ] = "minecraft:container";
+        inventoryTypes[ 1 ] = "minecraft:workbench";
+        inventoryTypes[ 2 ] = "minecraft:furnace";
+        inventoryTypes[ 3 ] = "minecraft:dispenser";
+        inventoryTypes[ 4 ] = "EnchantTable"; // Broken
+        inventoryTypes[ 5 ] = "minecraft:brewing_stand";
+        inventoryTypes[ 6 ] = "minecraft:villager";
+        inventoryTypes[ 7 ] = "minecraft:beacon";
+        inventoryTypes[ 8 ] = "minecraft:anvil";
+        inventoryTypes[ 9 ] = "minecraft:hopper";
+        inventoryTypes[ 11 ] = "EntityHorse";
+    }
 
     @Override
     public void rewrite(ByteBuf in, ByteBuf out)
     {
         out.writeByte( in.readByte() );
-        DefinedPacket.writeString( inventoryTypes[ in.readUnsignedByte() ], out );
+        short inventoryType = in.readUnsignedByte();
+        DefinedPacket.writeString( inventoryTypes[ inventoryType ], out );
+        DefinedPacket.writeString( "{translate:\"" + DefinedPacket.readString( in ) + "\"}", out );
+        //DefinedPacket.writeString( "{'translate':'tile.chest.name'}", out );
+        //in.skipBytes(  DefinedPacket.readVarInt( in ) );
+        out.writeByte( in.readByte() );
+        in.skipBytes( 1 );
         out.writeBytes( in.readBytes( in.readableBytes() ) );
     }
 }

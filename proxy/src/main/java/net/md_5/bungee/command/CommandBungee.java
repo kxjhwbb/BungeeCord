5a9248ef5c4c92851492d3b6bbf8c75c7144af0c
wbb <cgnbin@qq.com>
2014-08-08 18:16:36 +0800

    修改Bungee命令内容，修改生成项目时的版本号内容。

--- proxy/src/main/java/net/md_5/bungee/command/CommandBungee.java
+++ proxy/src/main/java/net/md_5/bungee/command/CommandBungee.java
@@ -16,6 +16,7 @@ public class CommandBungee extends Command
     @Override
     public void execute(CommandSender sender, String[] args)
     {
-        sender.sendMessage( ChatColor.BLUE + "This server is running BungeeCord version " + ProxyServer.getInstance().getVersion() + " by md_5" );
+        sender.sendMessage( ChatColor.GREEN + "专业优化强化BungeeCord，多版本支持，【QQ:284107809】  BungeeCord版权所有：原作者md_5， 本次构建ID："+ ProxyServer.getInstance().getVersion() );
+        //sender.sendMessage( ChatColor.BLUE + "This server is running BungeeCord version " + ProxyServer.getInstance().getVersion() + " by md_5" );
     }
 }
